4c761be14f8196cbcf40773282667c7d9924ec5d
changshanzhao <qq2395624242@163.com>
2024-02-17 19:40:35 +0800

    旅圈界面

---
+++ android/app/src/main/java/lvjian/zhaojunbo/LvQuan.java
@@ -0,0 +1,11 @@
+package lvjian.zhaojunbo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LvQuan extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lvquan);//调用旅圈界面
+    }}
--- android/app/src/main/java/lvjian/zhaojunbo/MainActivity.java
+++ android/app/src/main/java/lvjian/zhaojunbo/MainActivity.java
@@ -1,5 +1,41 @@
 package lvjian.zhaojunbo;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Logger;
+import com.getcapacitor.PluginLoadException;
+import com.getcapacitor.PluginManager;
 
-public class MainActivity extends BridgeActivity {}
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bridgeBuilder.setInstanceState(savedInstanceState);
+        getApplication().setTheme(com.getcapacitor.android.R.style.AppTheme_NoActionBar);
+        setTheme(com.getcapacitor.android.R.style.AppTheme_NoActionBar);
+        setContentView(com.getcapacitor.android.R.layout.bridge_layout_main);
+        PluginManager loader = new PluginManager(getAssets());
+
+        try {
+            bridgeBuilder.addPlugins(loader.loadPluginClasses());
+        } catch (PluginLoadException ex) {
+            Logger.error("Error loading plugins.", ex);
+        }
+
+        this.load();
+        Button ok = (Button) this.findViewById(com.getcapacitor.android.R.id.button10);
+        ok.setOnClickListener(new ButtonListener());
+    }
+    private class ButtonListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            Intent intent=new Intent(MainActivity.this,LvQuan.class);//设置切换对应activity
+            startActivity(intent);//开始切换
+        }
+    }
+
+}
